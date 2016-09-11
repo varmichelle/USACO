67c21f2f333befcb0813550591db1f10d9ee2923
Michelle Li <michellecarolynclark@gmail.com>
2016-09-11 14:45:43 -0700

    gitignore

---
+++ USACO Training/src/concom.java
@@ -0,0 +1,21 @@
+/*
+ID: michell26
+LANG: JAVA
+TASK: concom
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class concom {
+			
+	public static void main(String[] args) throws FileNotFoundException{
+		
+		Scanner in = new Scanner(new File("concom.in.txt"));
+		PrintStream out = new PrintStream(new File("concom.out.txt"));
+		
+		
+		
+	}
+
+}
