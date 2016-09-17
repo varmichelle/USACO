e63276b762b7edf2c9525ffe4311f636736a5601
Michelle Li <michellecarolynclark@gmail.com>
2016-09-17 08:13:39 -0700

    wall

--- Astar Gold Basic/src/wall.java
+++ Astar Gold Basic/src/wall.java
@@ -8,6 +8,7 @@ public class wall {
 		Scanner in = new Scanner(new File("wall.in.txt"));
 		PrintStream out = new PrintStream(new File("wall.out.txt"));
 		
+		// read input
 		int H = in.nextInt();
 		int F = in.nextInt();
 		Coordinate[] footholds = new Coordinate[F];
@@ -15,9 +16,13 @@ public class wall {
 			Coordinate c = new Coordinate(in.nextInt(), in.nextInt());
 		}
 		
+		
+		
 	}
 	
+	// checks if the two coordinates are within 1 meter apart
 	public static boolean isWithinDistance(Coordinate a, Coordinate b) {
+		// 1000000 mm^2 = 1 m^2
 		if (Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2)) <= 1000000) return true;
 		return false;
 	}
