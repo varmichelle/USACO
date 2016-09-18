fc3055ca0c6e277e764bcc087e0bb77ec5bb2a9a
Michelle Li <michellecarolynclark@gmail.com>
2016-09-18 11:18:02 -0700

    finished climbing wall problem

--- Astar Gold Basic/src/gravity.java
+++ Astar Gold Basic/src/gravity.java
@@ -1,8 +1,14 @@
+import java.util.*;
+import java.io.*;
 
 public class gravity {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner in = new Scanner(new File("gravity.in.txt"));
+		PrintStream out = new PrintStream(new File("gravity.out.txt"));
+		
+		
 
 	}
 
--- Astar Gold Basic/src/wall.java
+++ Astar Gold Basic/src/wall.java
@@ -2,8 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class wall {
-
+	
 	public static void main(String[] args) throws FileNotFoundException {
+		
+		/**
+		 * General solution idea:
+		 * Brute force BFS 
+		 * Optimize by only checking nodes within 1000 
+		 */
 		
 		Scanner in = new Scanner(new File("wall.in.txt"));
 		PrintStream out = new PrintStream(new File("wall.out.txt"));
@@ -11,28 +17,40 @@ public class wall {
 		// read input
 		int H = in.nextInt();
 		int F = in.nextInt();
-		Coordinate[] footholds = new Coordinate[F];
+		int[][] footholds = new int[F][2];
+		int[] dist = new int[F];
+		Queue<Integer> q = new LinkedList<Integer>();
 		for (int i = 0; i < F; i++) {
-			Coordinate c = new Coordinate(in.nextInt(), in.nextInt());
+			footholds[i][0] = in.nextInt();
+			footholds[i][1] = in.nextInt();
+			dist[i] = 99999999;
+			// push all legal starting positions into the queue
+			if (footholds[i][1] <= 1000) {
+				dist[i] = 1;
+				q.add(i);
+			}
 		}
-		
-		
+		while (!q.isEmpty()) {
+			// grab the index of the current foothold
+			int current = q.remove();
+			// if the current position is within 1000 mm of the top
+			if (H - footholds[current][1] <= 1000) {
+				System.out.println(dist[current]);
+				System.exit(0);
+			}
+			// for each foothold
+			for (int i = 0; i < F; i++) {
+				// if it's not the current foothold and we can get a better distance than previous
+				if (i != current && dist[current] + 1 < dist[i]) {
+					// if within 1000 mm
+					if (Math.sqrt(Math.pow(footholds[i][0] - footholds[current][0], 2) + Math.pow(footholds[i][1] - footholds[current][1], 2)) <= 1000) {
+						dist[i] = dist[current] + 1;
+						q.add(i);
+					}
+				}
+			}
+		}
 		
 	}
 	
-	// checks if the two coordinates are within 1 meter apart
-	public static boolean isWithinDistance(Coordinate a, Coordinate b) {
-		// 1000000 mm^2 = 1 m^2
-		if (Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2)) <= 1000000) return true;
-		return false;
-	}
-
-}
-
-class Coordinate {
-	int x, y;
-	Coordinate(int a, int b) {
-		x = a;
-		y = b;
-	}
 }
