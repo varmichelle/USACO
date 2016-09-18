6910b6739d8405af7700b3723d0f732a8cc53517
Michelle Li <michellecarolynclark@gmail.com>
2016-09-18 12:32:21 -0700

    gravity pt 1

--- Astar Gold Basic/src/gravity.java
+++ Astar Gold Basic/src/gravity.java
@@ -5,11 +5,43 @@ public class gravity {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
+		/**
+		 * General Solution Idea:
+		 * BFS implementing DFS to find all reachable points with the same flip
+		 */
+	
 		Scanner in = new Scanner(new File("gravity.in.txt"));
 		PrintStream out = new PrintStream(new File("gravity.out.txt"));
 		
-		
+		// read input
+		int N = in.nextInt();
+		int M = in.nextInt();
+		char[][] grid = new char[N][M];
+		int[][] flips = new int[N][M];
+		Struct start, end;
+		for (int i = 0; i < M; i++) {
+			String str = in.next();
+			for (int j = 0; j < N; j++) {
+				grid[j][i] = str.charAt(j);
+				if (grid[j][i] == 'C') start = new Struct(j, i);
+				else if (grid[j][i] == 'D') end = new Struct(j, i);			
+				else if (grid[j][i] == '#') flips[j][i] = -1;
+			}
+		}
+		Queue<Struct> q = new LinkedList<Struct>();
+		while (!q.isEmpty()) {
+			Struct current = q.remove();
+			
+		}
 
 	}
 
+}
+
+class Struct {
+	int x, y;
+	Struct(int a, int b) {
+		x = a;
+		y = b;
+	}
 }
