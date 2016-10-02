4bd4e87e240573f20e13220e72ba22f5d1b96b80
Michelle Li <michellecarolynclark@gmail.com>
2016-10-01 19:01:14 -0700

    fliptile

--- Astar Gold Basic/src/fliptile.java
+++ Astar Gold Basic/src/fliptile.java
@@ -3,8 +3,11 @@ import java.io.*;
 
 public class fliptile {
 	
+	static int rows, cols;
+	static int[][] grid, copy, flips;
+	
 	public static void main(String[] args) throws FileNotFoundException {
-				
+
 		/**
 		 * General solution: Try all possible values for flips of the first row
 		 * Based on what tiles still need to be flipped in the first row, 
@@ -16,18 +19,74 @@ public class fliptile {
 		// read input
 		Scanner in = new Scanner(new File("fliptile.in.txt"));
 		PrintStream out = new PrintStream(new File("fliptile.out.txt"));
-		int rows = in.nextInt();
-		int cols = in.nextInt();
-		int[][] grid = new int[cols][rows];
+		rows = in.nextInt();
+		cols = in.nextInt();
+		grid = new int[cols][rows];
+		copy = new int[cols][rows];
+		flips = new int[cols][rows];
 		for (int y = 0; y < rows; y++) {
 			for (int x = 0; x < cols; x++) {
 				grid[x][y] = in.nextInt();
+				copy[x][y] = grid[x][y];
+				flips[x][y] = 0;
 			}
 		}
 		
 		// generate all possible flip combinations for the first row
 		for (int num = 0; num < Math.pow(2, cols); num++) {
-			
+			String map = "000000000000000" + Integer.toBinaryString(num);
+			// generate the flips for the first row
+			map = map.substring(map.length() - cols, map.length());
+			for (int i = 0; i < cols; i++) {
+				if (map.charAt(i) == '1') flip(i,0);
+			}
+			// check the other rows
+			for (int j = 1; j < rows; j++) {
+				for (int i = 0; i < cols; i++) {
+					if (grid[i][j - 1] == '1') flip(i, j);
+				}
+			}
+			// check if the solution works
+			boolean works = true;
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					if (grid[j][i] == 1) works = false;
+				}
+			}
+			// if it does, print the results
+			if (works) {
+				for (int i = 0; i < rows; i++) {
+					for (int j = 0; j < cols; j++) {
+						System.out.print(flips[j][i]);
+						if (j < cols - 1) System.out.print(" ");
+					}
+					System.out.println();
+				}
+			}
+			reset();
+		}
+	}
+
+	// execute a flip (flip the tile itself + 4 adjacent tiles)
+	public static void flip(int x, int y) {
+		int[][] dir = {{0,-1},{0,1},{-1,0},{1,0}};
+		copy[x][y] = 1 - copy[x][y];
+		flips[x][y] = 1;
+		for (int i = 0; i < 4; i++) {
+			if (x + dir[i][0] >= 0 && x + dir[i][0] < cols) {
+				if (y + dir[i][1] >= 0 && y + dir[i][1] < rows) {
+					copy[x][y] = 1 - copy[x][y];
+				}
+			}
+		}
+	}
+	
+	public static void reset() {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				copy[j][i] = grid[j][i];
+				flips[j][i] = 0;
+			}
 		}
 	}
 
