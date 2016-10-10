4d87fa0566fcc2fd7a3ae143c6e9dd68d78f3f1c
Michelle Li <michellecarolynclark@gmail.com>
2016-10-09 21:07:51 -0700

    lookup

--- Astar Gold Basic/src/lookup.java
+++ Astar Gold Basic/src/lookup.java
@@ -5,10 +5,37 @@ public class lookup {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
+		// set up file readers and writers
 		Scanner in = new Scanner(new File("lookup.in.txt"));
 		PrintStream out = new PrintStream(new File("lookup.out.txt"));
 		
+		// read in input
+		int N = in.nextInt();
+		int[] heights = new int[N];
+		int[] lookups = new int[N];
+		for (int i = 0; i < N; i++) {
+			heights[i] = in.nextInt();
+		}
 		
+		// stores the indexes of the cows that need someone to look up to
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(0);
+		for (int i = 1; i < N; i++) {
+			while (!stack.isEmpty()) {
+				// if the current cow is taller than the previous cow in the stack, update lookups
+				if (heights[i] > heights[stack.peek()]) {
+					// add one because array index starts from 0 but given index starts from 1
+					lookups[stack.peek()] = i + 1;
+					stack.pop();
+				} else break;
+			}
+			stack.push(i);
+		}
+		
+		// print the results
+		for (int i = 0; i < N; i++) {
+			System.out.println(lookups[i]);
+		}
 
 	}
 
--- Astar Gold Basic/src/ttravel.java
+++ Astar Gold Basic/src/ttravel.java
@@ -1,8 +1,16 @@
+import java.util.*;
+import java.io.*;
 
 public class ttravel {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+	
+		// set up file readers and writers
+		Scanner in = new Scanner(new File("ttravel.in.txt"));
+		PrintStream out = new PrintStream(new File("ttravel.out.txt"));
+		
+		// read in input
+		
 
 	}
 
