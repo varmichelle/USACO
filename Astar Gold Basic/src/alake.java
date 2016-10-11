ae9a469c09a5115ac2b424b9e08681877d610a58
Michelle Li <michellecarolynclark@gmail.com>
2016-10-10 20:55:50 -0700

    alake input

--- Astar Gold Basic/src/alake.java
+++ Astar Gold Basic/src/alake.java
@@ -1,8 +1,15 @@
+import java.util.*;
+import java.io.*;
 
 public class alake {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		// set up file readers and writers
+		Scanner in = new Scanner(new File("alake.in.txt"));
+		PrintStream out = new PrintStream(new File("alake.out.txt"));
+		
+		// read in input
 
 	}
 
