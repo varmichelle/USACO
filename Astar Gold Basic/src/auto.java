7acfed2b97d40dfaf1ec6a25168486169aa2a2a0
Michelle Li <michellecarolynclark@gmail.com>
2016-10-19 20:19:41 -0700

    auto read in word queries

--- Astar Gold Basic/src/auto.java
+++ Astar Gold Basic/src/auto.java
@@ -14,8 +14,14 @@ public class auto {
 		int N = in.nextInt();
 		Prefix[] dictionary = new Prefix[W];
 		for (int i = 0; i < W; i++) dictionary[i] = new Prefix(i, in.next());
+		int[] nth = new int[N];
+		String[] word = new String[N];
+		for (int i = 0; i < N; i++) {
+			nth[i] = in.nextInt();
+			word[i] = in.next();
+		}
+		
 		Arrays.sort(dictionary);
-		for (int i = 0; i < W; i++) System.out.println(dictionary[i].prefix);
 		
 	}
 
@@ -28,6 +34,8 @@ class Prefix implements Comparable<Prefix> {
 		index = i;
 		prefix = p;
 	}
+	
+	// lets us sort the dictionary array by prefix
 	public int compareTo(Prefix x) {
 		return this.prefix.compareTo(x.prefix);
 	}
