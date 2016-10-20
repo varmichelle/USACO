f5ae65f1bc550e11de7540671cb57d8345958ecf
Michelle Li <michellecarolynclark@gmail.com>
2016-10-19 19:40:52 -0700

    auto dictionary

--- Astar Gold Basic/src/auto.java
+++ Astar Gold Basic/src/auto.java
@@ -12,41 +12,23 @@ public class auto {
 		// read in input
 		int W = in.nextInt();
 		int N = in.nextInt();
-		String[] dictionary = new String[W];
-		for (int i = 0; i < W; i++) dictionary[i] = in.next();
+		Prefix[] dictionary = new Prefix[W];
+		for (int i = 0; i < W; i++) dictionary[i] = new Prefix(i, in.next());
 		Arrays.sort(dictionary);
-		int[] indeces = new int[N];
-		String[] prefixes = new String[N];
-		for (int i = 0 ; i < N; i++) {
-			indeces[i] = in.nextInt();
-			prefixes[i] = in.next();
-		}
-		
-		// loop through each query 
-		for (int i = 0; i < N; i++) {
-			int first = 0, last = N - 1, middle = (first + last)/2;
-		    while (first <= last) {
-		    	if (dictionary[middle].compareTo(prefixes[i]) < 0) first = middle + 1;    
-		    	else if (dictionary[middle].substring(0, prefixes[i].length()).equals(prefixes[i])) {
-		    		int index = i;
-		    		while (dictionary[index].substring(0, prefixes[i].length()).equals(prefixes[i])) {
-		    			index--;
-		    			if (index < 0) {
-		    				index = 0;
-		    				break;
-		    			}
-		    		}
-		    		if (index != 0) index++;
-		    		
-		    	}
-		    	else {
-		    		last = middle - 1;
-				    middle = (first + last)/2;
-		    	}
-		   }
-		   
-		}
+		for (int i = 0; i < W; i++) System.out.println(dictionary[i].prefix);
 		
 	}
 
+}
+
+class Prefix implements Comparable<Prefix> {
+	int index;
+	String prefix;
+	public Prefix(int i, String p) {
+		index = i;
+		prefix = p;
+	}
+	public int compareTo(Prefix x) {
+		return this.prefix.compareTo(x.prefix);
+	}
 }
