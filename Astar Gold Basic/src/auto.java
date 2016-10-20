b66657310aca2bbfc7f42d566cffc7d6857fb52b
Michelle Li <michellecarolynclark@gmail.com>
2016-10-19 21:12:54 -0700

    auto

--- Astar Gold Basic/src/auto.java
+++ Astar Gold Basic/src/auto.java
@@ -14,14 +14,40 @@ public class auto {
 		int N = in.nextInt();
 		Prefix[] dictionary = new Prefix[W];
 		for (int i = 0; i < W; i++) dictionary[i] = new Prefix(i, in.next());
+		Arrays.sort(dictionary);
 		int[] nth = new int[N];
 		String[] word = new String[N];
 		for (int i = 0; i < N; i++) {
 			nth[i] = in.nextInt();
 			word[i] = in.next();
+			// binary search to find occurrence of prefix in dictionary
+			int low = 0, high = W - 1, middle = (low + high) / 2;
+			while (low < high) {
+				if (dictionary[middle].prefix.compareTo(word[i]) < 0) {
+					low = middle + 1;
+					middle = (low + high) / 2;
+				}
+		    	else if (dictionary[middle].prefix.length() >= word[i].length() && dictionary[middle].prefix.substring(0, word[i].length()).equals(word[i])) {
+		    		int index = middle;
+		    		while (dictionary[index].prefix.length() >= word[i].length() && dictionary[index].prefix.substring(0, word[i].length()).equals(word[i])) {
+		    			index--;
+		    			if (index < 0) {
+		    				index = 0;
+		    				break;
+		    			}
+		    		}
+		    		if (index != 0) index++;
+		    		if ((index + nth[i] - 1 < W) && dictionary[index].prefix.length() >= word[i].length() && dictionary[index + nth[i] - 1].prefix.substring(0, word[i].length()).equals(word[i])) {
+		    			System.out.println(dictionary[index + nth[i] - 1].index + 1);
+		    		} else System.out.println(-1);
+		    		break;
+		    	}
+		    	else {
+		    		high = middle - 1;
+				    middle = (low + high)/2;
+		    	}
+			}
 		}
-		
-		Arrays.sort(dictionary);
 		
 	}
 
