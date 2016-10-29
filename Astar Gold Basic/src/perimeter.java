04eedc2f3493bca1450c1b84ad8363304ad47bf5
Michelle Li <michellecarolynclark@gmail.com>
2016-10-29 16:00:15 -0700

    perimeter

--- Astar Gold Basic/src/perimeter.java
+++ Astar Gold Basic/src/perimeter.java
@@ -3,6 +3,10 @@ import java.io.*;
 
 public class perimeter {
 
+	static int N, perimeter = 0, min_x, min_y, max_x = 0, max_y = 0;
+	static Set<Coordinate> haybales = new HashSet<Coordinate>();
+	static Set<Coordinate> visited = new HashSet<Coordinate>();
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		// set up file readers and writers
@@ -10,13 +14,73 @@ public class perimeter {
 		PrintStream out = new PrintStream(new File("perimeter.out.txt"));
 		
 		// read in input
-		int N = in.nextInt();
-		int[][] pos = new int[N][2];
+		N = in.nextInt();
+		min_x = 999999999;
+		min_y = 999999999;
+		int start_x = 0, start_y = 0;
 		for (int i = 0; i < N; i++) {
-			pos[i][0] = in.nextInt();
-			pos[i][1] = in.nextInt();
+			int x = in.nextInt();
+			int y = in.nextInt();
+			haybales.add(new Coordinate(x,y));
+			max_x = Math.max(max_x, x);
+			min_x = Math.min(min_x, x);
+			max_y = Math.max(max_y, y);
+			min_y = Math.min(min_y, y);
+			if (x >= start_x) {
+				start_x = x;
+				start_y = y;
+			}
 		}
+		recurse(start_x+1, start_y);
+		System.out.println(perimeter);
 
 	}
+	
+	static void recurse(int x, int y) {
+		// if isolated, return (to stay close to the haybales)
+		if (isolated(x,y)) return;
+		else {
+			// if haybale, increment perimeter
+			if (haybales.contains(new Coordinate(x, y))) {
+				perimeter++;
+				return;
+			}
+			if (visited.contains(new Coordinate(x-min_x,y-min_y))) return;
+			else visited.add(new Coordinate(x-min_x,y-min_y));
+			recurse(x+1,y);
+			recurse(x-1,y);
+			recurse(x,y+1);
+			recurse(x,y-1);
+		}
+	}
+	
+	static boolean isolated(int x, int y) {
+		// check if any of the 8 neighbors are haybales
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (haybales.contains(new Coordinate(x+i, y+j))) return false;
+			}
+		}
+		return true;
+	}
+}
 
+class Coordinate {
+	int x, y;
+	public Coordinate(int a, int b) {
+		x = a;
+		y = b;
+	}
+	
+	@Override
+	public int hashCode() {
+		return x*3 + y;
+	}
+	
+	@Override
+	public boolean equals(Object a) {
+		Coordinate b = (Coordinate) a;
+		if (this.x == b.x && this.y == b.y) return true;
+		else return false;
+	}
 }
--- USACO Silver Past Problems/src/perimeter.java
+++ USACO Silver Past Problems/src/perimeter.java
@@ -9,9 +9,10 @@ public class perimeter {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		Scanner fileReader = new Scanner(new File("perimeter.in"));
-		fileWriter = new PrintStream(new File("perimeter.out"));
+		Scanner fileReader = new Scanner(new File("perimeter.in.txt"));
+		fileWriter = new PrintStream(new File("perimeter.out.txt"));
 
+		// input
 		int numBayhales = fileReader.nextInt();
 		int[][] coord = new int[numBayhales][2];
 		int largest = 0;
@@ -26,27 +27,12 @@ public class perimeter {
 			}
 		}
 		grid = new char[largest + 2][largest + 2];
+		// mark haybales with "*" on the grid
 		for (int i = 0; i < numBayhales; i++) {
 			grid[coord[i][0]][coord[i][1]] = '*';
 		}
 		floodfill(0, 0, largest);
-		/* for (int i = 0; i < largest + 2; i++) {
-			for (int j = 0; j < largest + 2; j++) {
-				System.out.print(grid[i][j]);
-			}
-			System.out.println();
-		} */
-		perimeter(largest, grid);
-//		for (int x = 0; x < largest + 2; x++) {
-//			for (int y = 0; y < largest + 2; y++) {
-//				if (grid[x][y] != '*') {
-//					floodfill(x, y, largest);
-//					perimeter(largest, grid);
-//					System.exit(0);
-//				}
-//			}
-//		}
-		
+		perimeter(largest, grid);		
 
 	}
 
@@ -86,6 +72,7 @@ public class perimeter {
 	}
 
 	public static void floodfill(int x, int y, int largest) {
+		// stop flood-filling if you're out of bounds
 		if (x < 0 || y < 0 || x > largest + 1 || y > largest + 1) {
 			return;
 		} else {
