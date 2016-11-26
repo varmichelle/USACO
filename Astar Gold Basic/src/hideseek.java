8d03446b3b5c98fdbd60651a1f1b21b04fad3c99
Michelle Li <michellecarolynclark@gmail.com>
2016-11-25 16:36:36 -0800

    hideseek

--- Astar Gold Basic/src/hideseek.java
+++ Astar Gold Basic/src/hideseek.java
@@ -11,11 +11,55 @@ public class hideseek {
 		int N = in.nextInt(); // number of barns
 		int M = in.nextInt(); // number of paths
 		
-		List<Map<Integer, Integer>> graph = new ArrayList<Map<Integer, Integer>>();
-		// bfs with the adjacency list until you visit all of them
-		// print the index of the smallest indexed barn with largest distance
+		ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < N; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+		for (int i = 0; i < M; i++) {
+			int node1 = in.nextInt() - 1;
+			int node2 = in.nextInt() - 1;
+			graph.get(node1).add(node2);
+			graph.get(node2).add(node1);
+		}
 		
+		Queue<Barns> q = new LinkedList<Barns>();
+		int[] distances = new int[N];
+		boolean[] visited = new boolean[N];
+		int bestDistance = -1, bestIndex = 0, number =0;
+		q.add(new Barns(0,0));
+		while (!q.isEmpty()) {
+			Barns current = q.remove();
+			int index = current.index;
+			int distance = current.distance;
+			visited[index] = true;
+			distances[index] = distance;
+			if (distance > bestDistance) {
+				bestDistance = distance;
+				bestIndex = index;
+				number = 1;
+			} else if (distance == bestDistance) {
+				number++;
+				bestIndex = Math.min(bestIndex, index);
+			}
+			for (int i : graph.get(index)) {
+				if (!visited[i]) {
+					Barns barn = new Barns(i, distance + 1);
+					visited[i] = true;
+					q.add(barn);
+				}
+			}
+		}
+		
+		System.out.println((bestIndex+1) + " " + bestDistance + " " + number);
 		
 	}
 
+}
+
+class Barns {
+	int index, distance;
+	public Barns(int i, int d) {
+		index = i;
+		distance = d;
+	}
 }
