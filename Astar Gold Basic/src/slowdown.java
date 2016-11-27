94c185d7bd87d9cc1c56d1c91a97e74a39382a8d
Michelle Li <michellecarolynclark@gmail.com>
2016-11-27 14:14:11 -0800

    initial files

---
+++ Astar Gold Basic/src/slowdown.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class slowdown {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		
+		
+	}
+
+}
---
+++ Astar Gold Basic/src/ttime.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class ttime {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		
+
+	}
+
+}
