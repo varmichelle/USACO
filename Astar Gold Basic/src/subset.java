ce25bfefdc6e740deeff821e3f506012d028d5fa
Michelle Li <michellecarolynclark@gmail.com>
2017-02-04 11:03:59 -0800

    subset partitioning bug fix

--- Astar Gold Basic/src/subset.java
+++ Astar Gold Basic/src/subset.java
@@ -10,19 +10,23 @@ public class subset {
 		
 		int N = in.nextInt();
 		
-		int[][] dp = new int[N*(N+1)/2+1][N+1];
-		for (int i = 1; i <= N; i++) dp[i][i]++;
+		if (N*(N+1)/2 % 2 == 1) {
+			System.out.println(0);
+			System.exit(0);
+		}
+		long[][] dp = new long[N*(N+1)/4+1][N+1];
+		for (int i = 1; i <= N; i++) dp[i][i] = 1;
 		for (int y = 2; y <= N; y++) {
-			for (int x = 1; x <= N*(N+1)/2; x++) {
+			for (int x = 1; x <= N*(N+1)/4; x++) {
 				// copy all cells from the above row
-				dp[x][y] = dp[x][y-1];
+				dp[x][y] += dp[x][y-1];
 			}
 			// try adding y
-			for (int x = 1; x <= N*(N+1)/2; x++) {
-				if (x-y > 0 && dp[x-y][y-1] > 0) dp[x][y]++;
+			for (int x = 1; x <= N*(N+1)/4; x++) {
+				if (x-y >= 0 && dp[x-y][y-1] > 0) dp[x][y]+= dp[x-y][y-1];
 			}
 		}
-		System.out.println(dp[N*(N+1)/2][N]);
+		System.out.println(dp[N*(N+1)/4][N]/2);
 		
 	}
 
