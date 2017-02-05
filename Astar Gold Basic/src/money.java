bb219eb3eea05710aa94eb9aeb3637cf45435cd1
Michelle Li <michellecarolynclark@gmail.com>
2017-02-05 07:30:59 -0800

    money bugfix

--- Astar Gold Basic/src/money.java
+++ Astar Gold Basic/src/money.java
@@ -14,29 +14,22 @@ public class money {
 		int[] coins = new int[N+1];
 		for (int i = 1; i <= N; i++) coins[i] = in.nextInt();
 		
-		int[][] dp = new int[V+1][N+1];
+		long[][] dp = new long[V+1][N+1];
 		// base case
-		for (int i = 1; i <= N; i++) {
-			for (int j = coins[i]; j <= V; j+= coins[i]) {
-				dp[j][i] = 1;
-			}
-		}
-		for (int y = 2; y <= N; y++) {
+		for (int i = 0; i <= N; i++) dp[0][i] = 1;
+		
+		for (int y = 1; y <= N; y++) {
 			for (int x = 1; x <= V; x++) {
 				// copy everything from previous row
 				dp[x][y] += dp[x][y-1];
 				// add coin y
 				for (int j = coins[y]; j <= V; j+= coins[y]) {
-					if (x-j >= 0) dp[j][y]+= dp[x-j][y-1];
+					if (x-j >= 0) dp[x][y]+= dp[x-j][y-1];
 				}
 			}
 		}
-		for (int y = 1; y <= N; y++) {
-			for (int x = 1; x <= V; x++) {
-				System.out.print(dp[x][y] + " ");
-			}
-			System.out.println();
-		}
+		
+		System.out.println(dp[V][N]);
 		
 	}
 
