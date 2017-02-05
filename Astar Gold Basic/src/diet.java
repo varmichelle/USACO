d12b345e9b75ad18036c9fbd45a933745136a486
Michelle Li <michellecarolynclark@gmail.com>
2017-02-04 22:16:56 -0800

    convert to boolean array

--- Astar Gold Basic/src/diet.java
+++ Astar Gold Basic/src/diet.java
@@ -14,22 +14,22 @@ public class diet {
 		int[] V = new int[N+1];
 		for (int i = 1; i <= N; i++) V[i] = in.nextInt();
 		
-		int[][] dp = new int[H+1][N+1];
+		boolean[][] dp = new boolean[H+1][N+1];
 		// base case: adding the first item
-		dp[V[1]][1] = 1;
+		dp[V[1]][1] = true;
 		
 		for (int y = 2; y <= N; y++) {
 			for (int x = 1; x <= H; x++) {
 				// copy all cells from above
 				dp[x][y] = dp[x][y-1];
 				// check if possible to add the yth item
-				if (x-V[y] >= 0) dp[x][y] = Math.max(dp[x][y], dp[x-V[y]][y-1] + V[y]);
+				if (x-V[y] >= 0) dp[x][y] = dp[x][y] || dp[x-V[y]][y-1];
 				// progressively fill with max of current and previous
-				dp[x][y] = Math.max(dp[x][y], dp[x-1][y]);
+				dp[x][y] = dp[x][y] || dp[x-1][y];
 			}
 		}
 		for (int i = H; i >= 0; i--) {
-			if (dp[i][N] > 0) {
+			if (dp[i][N]) {
 				System.out.println(i);
 				break;
 			}
