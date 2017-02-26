9245e8e30c97cfebc671531891f44a73e8c6f6c6
Michelle Li <michellecarolynclark@gmail.com>
2017-02-25 16:14:47 -0800

    add

---
+++ Astar Gold Basic/src/bullcow.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class bullcow {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner in = new Scanner(new File("bullcow.in.txt"));
+		PrintStream out = new PrintStream(new File("bullcow.out.txt"));
+		
+		int N = in.nextInt();
+		int K = in.nextInt();
+		int[] dp = new int[N]; 
+
+	}
+
+}
